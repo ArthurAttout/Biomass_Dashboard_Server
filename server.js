9a3adfd4a46a50aaec00ff824593d78a411da72b
Arthur Attout <arthur.attout@outlook.com>
2019-05-09 14:28:53 +0200

    Geoloc

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const WebSocket = require('ws');
 const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
+const getJSON = require('get-json')
 
 console.log("Attemtping to reach MYSQL")
+
+
 const connection = mysql.createConnection({
   host     : '192.168.1.67',
   user     : 'npm',
@@ -16,15 +19,33 @@ const websocketMap = new Map();
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+const geoloc_url_pref = 'https://api.opencagedata.com/geocode/v1/json?q='
+const geoloc_url_suff = '&key=c51e1cfb6b70497a94cb4d714cda5afa' 
+
 const queryReportsByName = `
 SELECT report.* from report
 inner join researcher on report.FK_Researcher = researcher.ID
-where researcher.username = 'JohnDoe'
+where researcher.username = ?
+`
+
+const queryImagesOfReport = `
+select report_image.path from report_image
+inner join report_to_image on report_image.id = report_to_image.FK_Image
+where report_to_image.FK_Report = ?
 `
 
 wss.on('connection', function incoming(ws) {
 	
 	console.log("New connection")
+	
+	ws.on('close', function close() {
+		console.log("Close -> delete entry in map");
+		websocketMap.delete(ws.id)
+		console.log("Current WS map : ")
+		websocketMap.forEach((v,k) => {
+			console.log("%s -> %s",k,JSON.stringify(v))
+		})
+	});
 	
 	ws.on('message', function incoming(message) {
 		console.log('received: %s', message);
@@ -53,23 +74,47 @@ wss.on('connection', function incoming(ws) {
 				break
 
 			case 'GET_REPORTS':
-				console.log("Received requests for reports from %s",JSON.stringify(websocketMap.get(ws.id)))
-				connection.query(queryReportsByName, function (error, results, fields) {
-					if (error) throw error;
-					console.log("Retrieved from database : " + results)
-					
+				usernameTarget = websocketMap.get(ws.id).username
+				console.log("Received requests for reports from %s",usernameTarget)
+				
+				const sendReports = function(reports){
 					ws.send(JSON.stringify({
 						"result":"OK_REPORTS",
-						"reports":results.map(r => ({
-							"id":r.id,
-							"status":r.status,
-							"submission_date":r.submission_date,
-							"latitude":r.latitude,
-							"longitude":r.longitude,
-							"comment":r.comment
-						}))
+						"reports":reports
 					}))
-				})
+				}
+				
+				const onReportsByName = function (error, resultsReport, fields){
+					if (error) throw error;
+					
+					var i = 0
+					var reportsToSend = []
+					resultsReport.forEach((r) => {
+						connection.query(queryImagesOfReport,[r.id], function (error, resultsImages, fields){
+							getJSON(geoloc_url_pref + r.latitude + "+" + r.longitude + geoloc_url_suff, function(error, response){
+								reportsToSend = reportsToSend.concat({
+									"id":r.id,
+									"status":r.status,
+									"submission_date":r.submission_date,
+									"latitude":r.latitude,
+									"longitude":r.longitude,
+									"place":response.results[0].formatted,
+									"comment":r.comment,
+									"images":resultsImages
+								})
+								
+								i++
+								if(i === resultsReport.length){
+									sendReports(reportsToSend)
+								}
+							})							
+						})
+					})
+					
+					
+				}
+				
+				connection.query(queryReportsByName,[usernameTarget], onReportsByName)
 				break
 		}
 	});
